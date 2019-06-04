18d2b58c3d7bc1d267e8b62d5de0d6af82debced
Walther <walcarher@gmail.com>
2019-06-04 07:05:54 -0700

    library creation

---
+++ lib/CMakeFiles/lib_hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib_hello.dir/hello.c.o"
+  "liblib_hello.pdb"
+  "liblib_hello.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lib_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
